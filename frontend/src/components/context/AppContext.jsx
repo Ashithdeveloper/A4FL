b0eb3bf177665aa0274993c62e35e97be8b921a1
Ashithdeveloper <ashithashith593@gmail.com>
2025-05-11 21:50:15 +0530

    Describe your change here

--- frontend/src/components/context/AppContext.jsx
+++ frontend/src/components/context/AppContext.jsx
@@ -7,19 +7,21 @@ import  AppContext  from "./context";
 const ContextProvider = (props) => {
   const [user, setUser] = useState(null);
   console.log("user", user);
-    const getMe = async () => {
-      try {
-        const res = await axios.get(`${apiUrl}/auth/getme`, {
-          withCredentials: true,
-        });
-        setUser(res.data);
-      } catch (error){
-        console.log(error);
-        setUser(null);
-      }
-    };
+  const getMe = async () => {
+    try {
+      const res = await axios.get(`${apiUrl}/auth/getme`, {
+        withCredentials: true,
+      });
+      setUser(res.data);
+    } catch (error) {
+      console.log(error);
+      setUser(null);
+    }
+  };
+  // for send props to child componentgit add .
+
   return (
-    <AppContext.Provider value={{ user, getMe,setUser }}>
+    <AppContext.Provider value={{ user, getMe, setUser }}>
       {props.children}
     </AppContext.Provider>
   );
