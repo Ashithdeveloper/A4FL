3185d0c5aa82f9354734711144ef7d19c1a71c51
Ashithdeveloper <ashithashith593@gmail.com>
2025-05-23 20:35:32 +0530

    66

--- backend/generateToken.js
+++ backend/generateToken.js
@@ -8,6 +8,7 @@ const generateToken = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
+    path: "/",
     secure: process.env.NODE_ENV !== "production",
   });
 };
