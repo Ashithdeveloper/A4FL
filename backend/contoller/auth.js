ef6120120dccb139768a682b06598abb09c682ee
Ashithdeveloper <ashithashith593@gmail.com>
2025-05-23 20:22:59 +0530

    jj

--- backend/contoller/auth.js
+++ backend/contoller/auth.js
@@ -133,8 +133,7 @@ export const login = async (req, res) => {
 };
 export const logout = async (req, res) => {
   try {
-   
-    res.clearCookie("jwt");
+    res.cookie("jwt", "", { maxAge: 0 });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log(error);
