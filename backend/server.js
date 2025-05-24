621bc429deb6fe264730d3011974365c97cc6f80
Ashithdeveloper <ashithashith593@gmail.com>
2025-05-24 14:23:52 +0530

    ooouu

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: ["https://a4fl.vercel.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
